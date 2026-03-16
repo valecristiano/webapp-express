ea73175bba47a28cf1ac56b40a88a9602a69857e
valecristiano <valeriamdcristiano@gmail.com>
2026-03-16 18:35:58 +0300

    router fix

--- routers/movies.js
+++ routers/movies.js
@@ -5,6 +5,6 @@ const moviesController = require("../controllers/moviesController");
 
 router.get("/", moviesController.index);
 
-// router.get("/:id", moviesController.show);
+router.get("/:id", moviesController.show);
 
 module.exports = router;
